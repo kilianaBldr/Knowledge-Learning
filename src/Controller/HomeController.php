eea702faa7fb8e3ab267a35790e2e25753c24b0e
Kiliana Blondron <kiliana.bldr@gmailcom>
2025-03-21 10:33:36 +0100

    ajout de la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
